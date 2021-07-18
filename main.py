7622531ad137425a401825f3c3623594f65a035c
AbinayaSankaran <abk050305@gmail.com>
2021-07-18 17:17:43 +0530

    changes in two file

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 # comments are fo rgit execution
 # commen
 # codeo
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+# from ml_utils import load_model, predict
+
+app = FastAPI(
+    title="Iris Predictor",
+    docs_url="/"
+)
--- test_app.py
+++ test_app.py
@@ -1,2 +1,11 @@
 # Comments 
 # test 
+from fastapi.testclient import TestClient
+from main import app
+
+
+def test_ping():
+    with TestClient(app) as client:
+        response = client.get("/ping")
+        assert response.status_code == 200
+        assert response.json() == {"ping":"pong"}
